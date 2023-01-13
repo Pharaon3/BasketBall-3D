a64d01254b7c999c8ba017635f0667139882e8e3
Pharaon3 <Pharaon313@outlook.com>
2023-01-13 10:21:53 -0600

    lower case

--- Js/Controller.js
+++ Js/Controller.js
@@ -170,7 +170,7 @@ function load() {
       handleEventData(data.d);
     }
   };
-  document.getElementById('link').setAttribute('href', '../Basketball-2D/index.html?eventId=' + eventId)
+  document.getElementById('link').setAttribute('href', '../basketball-2d/index.html?eventId=' + eventId)
 }
 function bounceBall() {
   if(!setTimer)return
